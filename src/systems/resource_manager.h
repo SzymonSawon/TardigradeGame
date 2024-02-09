6417687d2bf5ebf665fb840e0e9050a58a5a2e3d
icebox246 <iceboxpoczta@gmail.com>
2024-02-09 16:03:22 +0100

    Add ResouceManagerSystem

---
+++ src/systems/resource_manager.h
@@ -0,0 +1,48 @@
+#ifndef RESOURCE_MANAGER_H_
+#define RESOURCE_MANAGER_H_
+
+#include <raylib.h>
+
+#include "../utils/da.h"
+
+#define SPRITE_NOT_FOUND 0
+
+typedef struct {
+    char* filename;
+    Texture texture;
+} ResourceManager_TextureFile;
+
+typedef struct {
+    da_list(ResourceManager_TextureFile);
+} ResourceManager_TextureFiles;
+
+typedef struct {
+    char* sprite_name;
+    size_t texture_index;
+    Rectangle source_rect;
+} ResourceManager_Sprite;
+
+typedef struct {
+    da_list(ResourceManager_Sprite);
+} ResourceManager_Sprites;
+
+typedef struct {
+    char* root_path;
+    ResourceManager_TextureFiles texture_files;
+    ResourceManager_Sprites sprites;
+} ResourceManagerSystem;
+
+void resource_manager_system_init(ResourceManagerSystem* sys,
+                                  char* assets_path);
+
+void resource_manager_system_update(ResourceManagerSystem* sys);
+
+void resource_manager_system_deinit(ResourceManagerSystem* sys,
+                                    char* assets_path);
+
+size_t resource_manager_load_texture_file(ResourceManagerSystem* sys,
+                                          char* filename);
+
+size_t resource_manager_get_sprite(ResourceManagerSystem* sys, char* name);
+
+#endif
--- src/world.c
+++ src/world.c
@@ -5,11 +5,13 @@
 void world_init(World* world) {
     TraceLog(LOG_INFO, "Initializing World!");
     *world = (World){.debug_info_system = {.visible = false}};
+    resource_manager_system_init(&world->resource_manager_system, "assets");
 }
 
 void world_update(World* world) {
     {
         // updating systems
+        resource_manager_system_update(&world->resource_manager_system);
     }
 
     BeginDrawing();
@@ -22,4 +24,7 @@ void world_update(World* world) {
     EndDrawing();
 }
 
-void world_deinit(World* world) { TraceLog(LOG_INFO, "Deinitializing World!"); }
+void world_deinit(World* world) {
+    TraceLog(LOG_INFO, "Deinitializing World!");
+    resource_manager_system_init(&world->resource_manager_system, "assets");
+}
