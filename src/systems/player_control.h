75f9ce3ffa7ee12389decc4380a96b99dab3e9eb
SzymonSawon <szymonsawon4@gmail.com>
2024-02-14 11:42:36 +0100

    clang formatted

--- src/systems/player_control.h
+++ src/systems/player_control.h
@@ -6,27 +6,25 @@
 #include "../utils/da.h"
 #include "resource_manager.h"
 
-
 typedef struct {
-  int movement_speed;
-  int dodge_speed;
-  int jump_force;
-  Vector2 position;
-  enum {
-  LEFT,
-  RIGHT,
-  } direction;
-  enum {
-  WALK,
-  JUMP,
-  DODGE,
-  } state;
-  bool can_move;
+    int movement_speed;
+    int dodge_speed;
+    int jump_force;
+    Vector2 position;
+    enum {
+        LEFT,
+        RIGHT,
+    } direction;
+    enum {
+        WALK,
+        JUMP,
+        DODGE,
+    } state;
+    bool can_move;
 } Player_Info;
 
 void player_side_movement(Player_Info *player);
 void player_jump(Player_Info *player);
 void player_dodge(Player_Info *player);
-
 
 #endif
